c1946cf7e870d9dbf3b07f28c24188bbc145c64a
Maria Clara <claarasantana@gmail.com>
2018-06-20 03:23:09 -0300

    test getGames

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -1,3 +1,7 @@
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import games from '../__test-helpers__/games';
 import {
   START_FETCHING,
@@ -5,6 +9,26 @@ import {
   UPDATE_GAMES,
 } from '../actionTypes';
 import * as actions from './index';
+
+const mockStore = configureMockStore([ thunk ]);
+const store = mockStore({
+  error: false,
+  fetching: false,
+  games: [],
+});
+
+function mockFetch(data) {
+  return jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => data
+    })
+  );
+}
+
+function mockFailingFetch() {
+  return jest.fn().mockImplementation(() => Promise.reject({ error: 'ops, something went wrong' }));
+}
 
 describe('action creators', () => {
   it('startFetching should dispatch a START_FETCHING action', () => {
@@ -20,5 +44,31 @@ describe('action creators', () => {
   it('updateGames should dispatch an UPDATE_GAMES action', () => {
     expect(actions.updateGames(games))
       .toEqual({ type: UPDATE_GAMES, games });
+  });
+
+  afterEach(() => store.clearActions());
+
+  it('successful getGames calls startFetching and updateGames', async () => {
+    window.fetch = mockFetch(games);
+
+    const expectedActions = [
+      { type: START_FETCHING },
+      { type: UPDATE_GAMES, games },
+    ];
+
+    return store.dispatch(actions.getGames())
+      .then(() => expect(store.getActions()).toEqual(expectedActions))
+  });
+
+  it('failing getGames calls startFetching and throwError', async () => {
+    window.fetch = mockFailingFetch();
+
+    const expectedActions = [
+      { type: START_FETCHING },
+      { type: THROW_ERROR },
+    ];
+
+    return store.dispatch(actions.getGames())
+      .then(() => expect(store.getActions()).toEqual(expectedActions))
   });
 });
