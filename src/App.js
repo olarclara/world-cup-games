44ed06e1fa1b99a3c793ef34d0ac8a20d5e4f4e1
Maria Clara <claarasantana@gmail.com>
2018-06-20 02:50:20 -0300

    create basic App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const App = () => (
-  <div>
-    world cup app.
-  </div>
-);
+import { getGames } from './actions';
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    this.props.getGames();
+  }
+
+  render() {
+    const { games } = this.props;
+
+    return (
+      <React.Fragment>
+        {games && games.map(game => (
+          <div>{JSON.stringify(game)}</div>
+        ))}
+      </React.Fragment>
+    );
+  }
+};
+
+const mapStateToProps = state => ({
+  error: state.error,
+  fetching: state.fetching,
+  games: state.games,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getGames,
+}, dispatch);
+
+App.propTypes = {
+  error: PropTypes.bool.isRequired,
+  fetching: PropTypes.bool.isRequired,
+  games: PropTypes.array.isRequired,
+  getGames: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
